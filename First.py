c1211e433b0c821c06f3a569dae057a416cd70d8
Mateo Cattoni <matucattoni@gmail.com>
2023-03-10 07:31:52 -0300

    int

--- First.py
+++ First.py
@@ -5,6 +5,12 @@ def suma(n1,n2):
     total =n1+n2
     return total
 
+def res(n1,n2):
+    total =n1-n2
+    return total
+
 total = suma(n1,n2)
+total2 = res(n1,n2)
 
 print(total)
+print(total2)
